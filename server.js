4671dede07de8bf8e2eb2f474ea5f0b65ac7b1a5
yash261 <goelyashgoel30@gmail.com>
2021-06-05 13:13:26 +0530

    uplisten-api

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const http=require("http");
 const port=process.env.port || 3000;
 const app=require("./app");
 const server=http.createServer(app);
-server.listen(port);
+server.listen(port,() => console.log(`Server running on ${port}, http://localhost:${port}`));
